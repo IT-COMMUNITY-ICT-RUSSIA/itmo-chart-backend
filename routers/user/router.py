f9dc5c3526aa699e2511113fa7f8b9b3bd0365f9
arseniiarsenii <arseniivelichko2@gmail.com>
2021-12-17 20:38:26 +0300

    user router structure

---
+++ routers/user/router.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter
+
+user_router = APIRouter()
+
+
+@user_router.post("/login")
+def login():
+    """
+    log the user in provided the username
+    and password and return a JWT token
+    """
+    pass
+
+
+@user_router.get("/me")
+def get_user_data():
+    """return data for the requested user"""
+    pass
+
+
+@user_router.get("/users")
+def get_all_users():
+    """get all known users"""
+    pass
+
+
+@user_router.get("/achievements")
+def get_user_achievements():
+    """return achievement data for the requested user"""
+    pass
