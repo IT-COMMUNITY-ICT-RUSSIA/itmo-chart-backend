cbd3fb0ed4a7433f699a0c96849470408e572004
arseniiarsenii <arseniivelichko2@gmail.com>
2021-12-18 14:38:52 +0300

    implement database wrapper, change project structure

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import typing as tp
 from fastapi import FastAPI
 
-import routers
-
+from modules import routers, database
 
 api = FastAPI()
+DB = database.MongoDbWrapper()
 
 
 @api.get("/")
@@ -12,6 +12,6 @@ def get_status() -> tp.Dict[str, str]:
     return {"ok": True}
 
 
-api.include_router(router=routers.service_router, prefix="/service", tags=["Service Endpoints"])
+# api.include_router(router=routers.service_router, prefix="/service", tags=["Service Endpoints"])
 api.include_router(router=routers.user_router, prefix="/user", tags=["User Management Enpoints"])
 api.include_router(router=routers.chart_router, tags=["Chart Endpoints"])
--- routers/__init__.py
+++
@@ -1,3 +0,0 @@
-from presentator.router import chart_router
-from user.router import user_router
-from service.router import service_router
