260456a9f9696febacd6baa8df2cd6d56913f7af
timofeev41 <timofeevnik41@gmail.com>
2021-12-18 16:04:00 +0300

    :zap: user management endpoints

--- modules/routers/user/router.py
+++ modules/routers/user/router.py
@@ -1,4 +1,20 @@
+from loguru import logger
 from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from modules.database import MongoDbWrapper
+
+from modules.models import Token
+from modules.routers.user.models import User, UserOut, UsersOut
+
+from ...security import (
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+    authenticate_user,
+    create_access_token,
+    get_current_user,
+)
+from ...exceptions import AuthException
+from datetime import timedelta
+
 
 user_router = APIRouter(
     prefix="/user",
@@ -7,27 +23,35 @@ user_router = APIRouter(
 
 
 @user_router.post("/login")
-def login():
+async def login(form_data: OAuth2PasswordRequestForm = Depends()) -> Token:
     """
     log the user in provided the username
     and password and return a JWT token
     """
-    pass
+    user = await authenticate_user(form_data.username, form_data.password)
+    if not user:
+        logger.warning(f"Failed to login user {form_data.username}")
+        raise AuthException
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
 
 
 @user_router.get("/me")
-def get_user_data():
+async def get_user_data(user: User = Depends(get_current_user)) -> UserOut:
     """return data for the requested user"""
-    pass
+    return user
 
 
 @user_router.get("/users")
-def get_all_users():
+async def get_all_users(user: User = Depends(get_current_user)) -> UsersOut:
     """get all known users"""
-    pass
+    return await MongoDbWrapper().get_all_users()
 
 
 @user_router.get("/achievements")
-def get_user_achievements():
+async def get_user_achievements(user: User = Depends(get_current_user)):
     """return achievement data for the requested user"""
-    pass
+    return await MongoDbWrapper().get_all_recieved_achievements_for_user(user=user)
